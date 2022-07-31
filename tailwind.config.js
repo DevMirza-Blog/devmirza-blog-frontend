a2a79529dafc6bc2be49db349c44fb7f26802bd3
Zaid-maker <pzhafeez@gmail.com>
2022-07-31 15:04:14 +0500

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#53BD95",
+        "primary-dark": "#2c785c",
+      },
+    },
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
   },
   plugins: [],
 };
