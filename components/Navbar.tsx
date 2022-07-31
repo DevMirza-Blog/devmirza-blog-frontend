96b366b116720f219b1cbd1c2ed14667a716b2d3
Zaid-maker <pzhafeez@gmail.com>
2022-07-31 14:57:07 +0500

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,35 @@ const Navbar = () => {
           <span className="font-bold ml-2 text-primary">DevMirza Blog</span>
         </div>
       </Link>
+      <ul className="flex items-center">
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#">Products</a>
+        </li>
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#">pricing</a>
+        </li>
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#">Docs</a>
+        </li>
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#">Company</a>
+        </li>
+      </ul>
+      <ul className="flex items-center">
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#" className="hover:text-gray-400">
+            Log in (Not Implemented)
+          </a>
+        </li>
+        <li className="font-medium text-gray-600">
+          <a
+            href="#"
+            className="bg-primary py-2 px-4 rounded-sm text-white hover:bg-primary-dark transition-all"
+          >
+            Sign up (Not implemented)
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 };
