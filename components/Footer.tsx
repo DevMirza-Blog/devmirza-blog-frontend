bd4fc462d2af326cd4ca0be61c6d7f8ebb1fbdc4
Zaid-maker <pzhafeez@gmail.com>
2022-08-01 13:38:44 +0500

    as suggested

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="text-gray-600 mt-12 fixed bottom-0 left-0 bg-white w-full">
       <div className="container mx-auto flex items-center flex-col sm:flex-row py-8">
-        <a
+        <Link
           href="/"
           className="flex items-center justify-center md:justify-start text-gray-900"
         >
           <span className="ml-3 text-md">DevMirza Blog</span>
-        </a>
+        </Link>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © 2022 DevMirza Blog — All rights reserved —
           <a
