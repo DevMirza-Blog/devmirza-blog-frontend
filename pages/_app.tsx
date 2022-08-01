9c57d5d0a7d8ed52f698cc03d88f61ee89bf0350
Zaid-maker <pzhafeez@gmail.com>
2022-08-01 13:32:13 +0500

    Footer :D

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <main className="pb-32">
           <Component {...pageProps} />
         </main>
+        <Footer />
       </div>
     </>
   );
