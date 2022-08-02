408319ee38d4e004eeb1c7900a388baa7aa2f49e
Zaid-maker <pzhafeez@gmail.com>
2022-08-02 22:41:44 +0500

    dont know why builds are failing

--- components/ArticleList.tsx
+++ components/ArticleList.tsx
@@ -1,28 +1,20 @@
-import React from "react";
-import { IArticle } from "../types";
-import Blogcard from "./Blogcard";
-import BlogCardWithImage from "./BlogCardWithImage";
+import React from 'react'
+import { IArticle } from '../types'
+import Blogcard from './Blogcard'
+import BlogCardWithImage from './BlogCardWithImage'
 
 interface IPropType {
-  articles: IArticle[];
+  articles: IArticle[]
 }
 
 const ArticleList = ({ articles }: IPropType) => {
   return (
     <div className="grid lg:grid-cols-2 grid-gap gap-16 mt-16">
       {articles.map((article, idx) => {
-        return (
-          <div key={article.id}>
-            {idx === 1 ? (
-              <BlogCardWithImage article={article} />
-            ) : (
-              <Blogcard article={article} />
-            )}
-          </div>
-        );
+        return <Blogcard article={article} key={article.id} />
       })}
     </div>
-  );
-};
+  )
+}
 
-export default ArticleList;
+export default ArticleList
--- components/Blogcard.tsx
+++ components/Blogcard.tsx
@@ -1,12 +1,25 @@
-import React from "react";
-import { IArticle } from "../types";
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { IArticle } from '../types'
 
 interface IPropType {
-  article: IArticle;
+  article: IArticle
 }
 
 const BlogCard = ({ article }: IPropType) => {
-  return <div>BlogCard</div>;
-};
+  return (
+    <div>
+      <Link href={`/article/${article.attributes.Slug}`}>
+        <h1 className="text-xl text-gray-600 font-bold hover:decoration-2 hover:underline hover:cursor-pointer hover:decoration-primary">
+          {article.attributes.Title}
+        </h1>
+      </Link>
+      <div className="flex items-center my-4">
+        <div></div>
+      </div>
+    </div>
+  )
+}
 
-export default BlogCard;
+export default BlogCard
