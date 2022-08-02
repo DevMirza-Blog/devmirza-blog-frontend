835f1f5fea7b30b8c853af8ef526e3cfcc4f2c3f
Zaid-maker <pzhafeez@gmail.com>
2022-08-02 16:24:16 +0500

    ahhh

--- utils/index.ts
+++ utils/index.ts
@@ -1,48 +1,45 @@
-import { serialize } from "next-mdx-remote/serialize";
-import { IArticle } from "../types";
+import { serialize } from 'next-mdx-remote/serialize'
+import { IArticle } from '../types'
 
-export const formatData = (dateString: string): string => {
-  const data = new Date(dateString).toLocaleDateString("en-US", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-
-  return data;
-};
+export const formatDate = (dateString: string): string => {
+  const date = new Date(dateString).toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  })
+  return date
+}
 
 export const makeCategory = (slug: string): string => {
-  if (typeof slug === "string") {
-    return slug.split("-").join(" ");
+  if (typeof slug === 'string') {
+    return slug.split('-').join(' ')
   }
-
-  return "";
-};
+  return ''
+}
 
 export const capitalizeFirstLetter = (str: string): string => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
 
 export const debounce = (fn: (query: string) => void, timeout = 300) => {
-  let timer: NodeJS.Timeout;
+  let timer: NodeJS.Timeout
   const debounced = (...args: any) => {
-    clearTimeout(timer);
+    clearTimeout(timer)
     timer = setTimeout(() => {
-      fn.apply(this, args);
-    }, timeout);
-  };
-
-  return debounced;
-};
+      fn.apply(this, args)
+    }, timeout)
+  }
+  return debounced
+}
 
 export const serializeMarkdown = async (item: IArticle) => {
-  const body = await serialize(item.attributes.body as string);
+  const body = await serialize(item.attributes.body as string)
   return {
     ...item,
     attributes: {
       ...item.attributes,
       body,
     },
-  };
-};
+  }
+}
