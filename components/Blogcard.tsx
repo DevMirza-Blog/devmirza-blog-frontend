40e3c42fe71796c2fa5e43d8281383a35d94e93d
Zaid-maker <pzhafeez@gmail.com>
2022-08-03 20:47:00 +0500

    complete the blog card

--- components/Blogcard.tsx
+++ components/Blogcard.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { IArticle } from '../types'
+import { formatDate } from '../utils'
 
 interface IPropType {
   article: IArticle
@@ -16,18 +17,25 @@ const BlogCard = ({ article }: IPropType) => {
         </h1>
       </Link>
       <div className="flex items-center my-4">
-        <div>
+        <div className="rounded-lg overflow-hidden flex items-center justify-center mr-2">
           <Image
-            src={`${process.env.API_BASE_URL}${article.attributes.author.data.attributes.avatar.data.attributes.formats.thumbnail.url}`}
+            src={`http://localhost:1337${article.attributes.author.data.attributes.avatar.data.attributes.formats.thumbnail.url}`}
             height={40}
             width={40}
           />
         </div>
-        <span>
+        <span className="text-sm font-bold text-gray-600">
           {article.attributes.author.data.attributes.firstname}{' '}
-          {article.attributes.author.data.attributes.lastname} on
-          <span className="text-gray-400"> {article.attributes.createdAt}</span>
+          {article.attributes.author.data.attributes.lastname} on &nbsp;
+          <span className="text-gray-400">
+            {formatDate(article.attributes.createdAt)}
+          </span>
         </span>
+      </div>
+      <div className="text-gray-500">
+        {article.attributes.shortDescription.slice(0, 250)}
+        {''}
+        {article.attributes.shortDescription.length > 250 ? '...' : ''}
       </div>
     </div>
   )
