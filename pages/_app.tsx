6e5309b1a7dfd1bd812cc96e6ce7d12cfbc32ef0
Zaid-maker <pzhafeez@gmail.com>
2022-08-04 17:44:53 +0500

    progress bar and category dynamic route

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import NextNProgress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <div className="container mx-auto font-sans">
+        <NextNProgress color="#53BD95" />
         <Navbar />
         <main className="pb-32">
           <Component {...pageProps} />
@@ -14,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Footer />
       </div>
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
---
+++ pages/category/[category].tsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import Head from 'next/head'
+import { capitalizeFirstLetter, debounce, makeCategory } from '../../utils'
+import {
+  IArticle,
+  ICategory,
+  ICollectionResponse,
+  IPagination,
+  IQueryOptions,
+} from '../../types'
+import Tabs from '../../components/Tabs'
+import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
+import { fetchArticles, fetchCategories } from '../../http'
+import { AxiosResponse } from 'axios'
+import qs from 'qs'
+import ArticleList from '../../components/ArticleList'
+import Pagination from '../../components/Pagination'
+
+interface IPropType {
+  categories: {
+    items: ICategory[]
+    pagination: IPagination
+  }
+  articles: {
+    items: IArticle[]
+    pagination: IPagination
+  }
+  slug: string
+}
+
+const category = ({ categories, articles, slug }: IPropType) => {
+  const router = useRouter()
+  const { category: categorySlug } = router.query
+  const { page, pageCount } = articles.pagination
+
+  const handleSearch = (query: string) => {
+    router.push(`/category/${categorySlug}/?search=${query}`)
+  }
+
+  const formattedCategory = () => {
+    return capitalizeFirstLetter(makeCategory(slug))
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Coder's Blog | {formattedCategory()}</title>
+        <meta name="description" content="DevMirza Blog" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Tabs
+        categories={categories.items}
+        handleOnSearch={debounce(handleSearch, 500)}
+      />
+      <ArticleList articles={articles.items} />
+      <Pagination
+        page={page}
+        pageCount={pageCount}
+        redirectUrl={`/category/${categorySlug}`}
+      />
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const options: IQueryOptions = {
+    populate: ['author.avatar'],
+    sort: ['id:desc'],
+    filters: {
+      category: {
+        slug: query.category,
+      },
+    },
+    pagination: {
+      page: query.page ? +query.page : 1,
+      pageSize: 1,
+    },
+  }
+
+  if (query.search) {
+    options.filters = {
+      Title: {
+        $containsi: query.search,
+      },
+    }
+  }
+
+  const queryString = qs.stringify(options)
+
+  const { data: articles }: AxiosResponse<ICollectionResponse<IArticle[]>> =
+    await fetchArticles(queryString)
+
+  const { data: categories }: AxiosResponse<ICollectionResponse<ICategory[]>> =
+    await fetchCategories()
+
+  return {
+    props: {
+      categories: {
+        items: categories.data,
+        pagination: categories.meta.pagination,
+      },
+      articles: {
+        items: articles.data,
+        pagination: articles.meta.pagination,
+      },
+      slug: query.category,
+    },
+  }
+}
+
+export default category
