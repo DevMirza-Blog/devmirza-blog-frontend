651e30bb06c5c82d945d9eb5bf8313f44dd4715b
Zaid-maker <pzhafeez@gmail.com>
2023-03-19 23:27:20 +0500

    🚀 chore(_app.tsx): update Analytics import to use @vercel/analytics/react instead of @vercel/analytics/next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { Session } from 'next-auth'
 
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
-import { Analytics } from '@vercel/analytics/next'
+import { Analytics } from '@vercel/analytics/react'
 
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
