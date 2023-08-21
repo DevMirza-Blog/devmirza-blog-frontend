cb463d85954685c6fef87a1a0ef9d0f41b4c4235
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-22 02:11:58 +0500

    Update process.d.ts

--- process.d.ts
+++ process.d.ts
@@ -6,11 +6,5 @@ declare namespace NodeJS {
     API_BASE_URL: string
     GITHUB_ID: string
     GITHUB_SECRET: string
-    GOOGLE_ID: string
-    GOOGLE_SECRET: string
-    DISCORD_ID: string
-    DISCORD_SECRET: string
-    FACEBOOK_ID: string
-    FACEBOOK_SECRET: string
   }
 }
